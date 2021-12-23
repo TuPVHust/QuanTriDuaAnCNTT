8e1ce2b12e52ce8541448246450a21ff4a98a21f
learnningforme@gmail.com <learnningforme@gmail.com>
2021-12-23 14:19:01 +0700

    thay doi gitino

--- abc-laravel8-master/resources/views/livewire/saleoff-product.blade.php
+++ abc-laravel8-master/resources/views/livewire/saleoff-product.blade.php
@@ -18,7 +18,7 @@
               <ul class="product__item__pic__hover">
                 <li><a href="#"><i class="fa fa-heart"></i></a></li>
                 <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                <li><a href="#" wire:click.prevent="store({{$product->id}},'{{$product->ten}}', {{$product->giaban}})">
+                <li><a href="javascript:void(0)" wire:click.prevent="store({{$product->id}},'{{$product->ten}}', {{$product->giaban}})">
                     <i class="fa fa-shopping-cart"></i></a>
                 </li>
               </ul>
